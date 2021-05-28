9b1f3eb478435c87a272e31e95bb0938e3e13789
Ashish Chaubey <ashishchaubey866@gmail.com>
2021-05-28 15:56:40 +0530

    debugBanner removed

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "BMI Calculator",
       theme: ThemeData(
           primaryColor: Color(0xFF0A0E21),
